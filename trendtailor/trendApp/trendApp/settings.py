6d26fa4f380ab46c6e08a8d3d54f0d21d92bee82
Bonnieeeyh <bonnieeeyh@gmail.com>
2025-03-11 00:42:40 -0400

    bonnie share news view

--- trendtailor/trendApp/trendApp/settings.py
+++ trendtailor/trendApp/trendApp/settings.py
@@ -137,3 +137,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "sahilrashid24@gmail.com"  # Replace with your Gmail
+EMAIL_HOST_PASSWORD = "pass123"  # Not your normal password, use an App Password!
